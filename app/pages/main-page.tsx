017f61038dcc235d352028c1295c9bca33229c2b
Arshia Esmailzadeh <arshiaas1973@gmail.com>
2026-01-06 00:58:50 +0330

    Added api gateways, db schema and adding section for words.

--- app/pages/main-page.tsx
+++ app/pages/main-page.tsx
@@ -1,15 +1,53 @@
+import { Icon } from "@iconify/react";
+import { useState } from "react";
+
 export default function MainPage() {
+    const [mode, setMode] = useState(0);
     return (
         <div className="w-full min-h-screen h-screen flex flex-col justify-center items-center">
             <div className="flex flex-col gap-6 items-center">
-                <div className="flex flex-col items-center gap-3">
-                    <h2 className="text-[36px] font-medium font-russian">Идтй</h2>
-                    <h3 className="text-2xl font-medium font-general">To go</h3>
-                    <h3 className="text-xl font-medium font-persian">رفتن</h3>
-                    <p className="text-base font-general max-w-[min(1000px,(100%-100px))]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, dolores vero? Tenetur, quasi nostrum autem non illum exercitationem porro inventore repudiandae pariatur recusandae vitae quia. Illo dolore exercitationem id atque.</p>
-                </div>
-                <button className="px-4 py-1.5 rounded-lg bg-sky-600/80 w-fit hover:bg-sky-600/55 cursor-pointer transition-colors duration-300 ease-linear">
-                    Learned it!
+                {
+                    {
+                        0: (
+                            <>
+                                <div className="flex flex-col items-center gap-3">
+                                    <h2 className="text-[36px] font-medium font-russian">Идтй</h2>
+                                    <h3 className="text-2xl font-medium font-general">To go</h3>
+                                    <h3 className="text-xl font-medium font-persian">رفتن</h3>
+                                    <p className="text-base font-general max-w-[min(1000px,(100%-100px))]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, dolores vero? Tenetur, quasi nostrum autem non illum exercitationem porro inventore repudiandae pariatur recusandae vitae quia. Illo dolore exercitationem id atque.</p>
+                                </div>
+                                <button className="px-4 py-1.5 rounded-lg bg-sky-600/80 w-fit hover:bg-sky-600/55 cursor-pointer transition-colors duration-300 ease-linear">
+                                    Learned it!
+                                </button>
+                            </>
+                        ),
+                        1: (
+                            <>
+                                <div className="flex flex-col gap-4">
+                                    <div className="flex flex-row gap-3">
+                                        <h2 className="font-general font-semibold text-[36px]">
+                                            Word :
+                                        </h2>
+                                        <input type="text" className="text-[36px] font-medium font-russian outline-none ring-0 focus:ring-0 focus:outline-white focus:outline-1" placeholder="Введите здесь..."/>
+                                    </div>
+                                </div>
+                            </>
+                        )
+                    }[mode]
+                }
+            </div>
+            <div className="flex flex-row items-center absolute top-4 start-4 w-fit h-fit">
+                <button className="cursor-pointer hover:text-sky-500 transition-colors duration-300 ease-linear" onClick={()=>{
+                    console.log("mode",mode)
+                    if(mode < 1){
+                        setMode((state) => state+1);
+                        return;
+                    }
+                    console.log("mode",mode)
+
+                    setMode(0);
+                }}>
+                    <Icon icon="line-md:cog-loop" className="size-6" />
                 </button>
             </div>
         </div>
--- backend/server.ts
+++ backend/server.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import
 import bodyParser from "body-parser";
 import cors from "cors";
 import dotenv from "dotenv";
+import type { Request, Response } from "electron";
+import { PrismaClient, type Translation } from "./generated/prisma/client";
 
 dotenv.config();
 
@@ -9,7 +12,63 @@ const app = express();
 app.use(bodyParser);
 app.use(cors({
     allowedHeaders: ["X-API-Key"],
-    methods: ["POST","GET","PUT","DELETE"],
+    methods: ["POST", "GET", "PUT", "DELETE"],
     origin: [process.env.FRONTEND_ORIGIN ?? "http://localhost:5173"],
 }));
+
+app.get("/api/v1/flashcards/random", async (req, res) => {
+    const prisma = new PrismaClient();
+    const count = await prisma.flashcard.count();
+    const skip = Math.floor(Math.random() * count);
+    const result = await prisma.flashcard.findMany({
+        take: 5,
+        skip: skip,
+    });
+    await res.status(200).json({
+        status: "success",
+        result,
+    });
+});
+
+app.post("/api/v1/flashcards/", async (req, res) => {
+    const {
+        word = "",
+        description = "",
+        translations = null,
+    }: {
+        translations: Translation[] | null,
+        word: string,
+        description: string,
+    } = await req.body;
+
+    if (word == "" || description == "" || (translations?.length ?? 0) <= 0) {
+        return await res.status(400).json({
+            status: "failed",
+            message: "Bad request parameters",
+        });
+    }
+
+    const prisma = new PrismaClient();
+    const flashcards = await prisma.flashcard.create({
+        data: {
+            word,
+            description,
+            translations: {
+                create: translations as Translation[],
+            },
+        },
+    });
+    if (!!!flashcards) {
+        await prisma.$disconnect();
+        return await res.status(409).json({
+            status: "failed",
+            result: "Failed to add flashcard",
+        })
+    }
+    await prisma.$disconnect();
+    return await res.status(200).json({
+        status: "success",
+        result: "Flashcard added successfully",
+    });
+});
 
